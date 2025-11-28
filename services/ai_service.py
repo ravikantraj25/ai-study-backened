2589f29efdb90a56cb8480b6f55eb9d487f5ddc9
Ravikant Raj <ravikantraj384@gmail.com>
2025-11-29 01:58:42 +0530

    fixed groq model error - updated deprecated model

--- services/ai_service.py
+++ services/ai_service.py
@@ -116,9 +116,10 @@ def summarize_text(text):
     """
 
     response = client.chat.completions.create(
-        model="llama-3.1-70b-versatile",
-        messages=[{"role": "user", "content": prompt}]
-    )
+    model="llama-3.3-70b-versatile",
+    messages=[{"role": "user", "content": prompt}]
+)
+
 
     return response.choices[0].message.content
 
